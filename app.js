7e1353fdeaf9acc7d1b5203db308726aa3458481
Ubuntu <ubuntu@ip-172-31-14-146.ca-central-1.compute.internal>
2024-01-14 05:22:12 +0000

    my-express-app files

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Define a basic route
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
