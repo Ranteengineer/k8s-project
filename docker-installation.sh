411f4fb317b53feee22e8027c51d7e327ce2457d
Debbie Ogundare <oluwaranti@Debbies-MBP.attlocal.net>
2024-01-14 16:50:15 -0600

    docker-installation.sh updated

--- docker-installation.sh
+++ docker-installation.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Add Docker's official GPG key:
 sudo apt-get update
 sudo apt-get install ca-certificates curl gnupg
@@ -8,16 +7,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 # Create  docker user
 sudo adduser DB
-#Add Docker user to Docker group to have elevated priviledge
-sudo usermod -aG docker DB  && newgrp docker
 
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
 
-#Install Docker packages
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
- 
+
